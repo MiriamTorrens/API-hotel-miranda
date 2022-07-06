5400713df3fb5adf3d255e6729e8eb8d9817a7e2
Miriam Torrens <m.torrens14@gmail.com>
2022-07-06 10:10:59 +0200

    > sorted

--- NODE/index.js
+++ NODE/index.js
@@ -11,11 +11,11 @@ const countriesList =
     "Country,Population,Area,Density\n"+
     countries
         //Dividir cada elemento del array en objeto: nombre, poblacion, area, densidad
-        .map(c => c.split(" "))
         .map(c => {
-            const name = c.splice(0, (c.length - 2)).join().replace(/,/g, ' ');
-            const population = parseFloat(c[c.length - 2]?.replace(/,/g, ''));
-            const area = parseFloat(c[c.length - 1]?.replace(/,/g, ''));
+            const splitData = c.split(" ")
+            const name = splitData.splice(0, (splitData.length - 2)).join().replace(/,/g, ' ');
+            const population = parseFloat(splitData[splitData.length - 2]?.replace(/,/g, ''));
+            const area = parseFloat(splitData[splitData.length - 1]?.replace(/,/g, ''));
             
             const country = {
                 name: name,
@@ -26,10 +26,8 @@ const countriesList =
             return country;
         })
         //Ordenar el array por densidad de población
-        .sort((a, b) => a.density - b.density)
+        .sort((a, b) => b.density - a.density)
         //Convertir el objeto country y el array countriesList en string para crear archivo csv
         .map(c => Object.values(c).toString()).join("\n");
 
 fs.writeFileSync("countries.csv", countriesList);
-
-console.log(countriesList);
