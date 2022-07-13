674abeaa9ae900530fb689c17d19463d1c3dfec9
Miriam Torrens <m.torrens14@gmail.com>
2022-07-13 17:41:22 +0200

    bookings controller ok

--- EXPRESS/controllers/bookings.controller.js
+++ EXPRESS/controllers/bookings.controller.js
@@ -1,33 +1,81 @@
-const bookings = require("../data/bookings.json");
+const { connection } = require("../db");
 
 exports.bookingsList = (req, res) => {
-  return res.json(bookings);
+  connection.query("SELECT * FROM bookings", (err, results) => {
+    if (err) throw err;
+    return res.json({ bookings: results });
+  });
 };
+
 exports.addBooking = (req, res) => {
-  bookings.push(req.body);
-  return res.json({ success: true, message: "Booking successfully added" });
+  const newBooking = [
+    req.body.guest_name,
+    req.body.order_date,
+    req.body.checkin,
+    req.body.checkout,
+    req.body.special_request,
+    req.body.room_id,
+    req.body.status,
+  ];
+  connection.query(
+    "INSERT INTO bookings (guest_name, order_date, checkin, checkout, special_request, room_id, status) VALUES (?)",
+    [newBooking],
+    (err, results) => {
+      if (err) throw err;
+      return res.json({
+        success: true,
+        message: "Booking successfully added",
+      });
+    }
+  );
 };
+
 exports.getBooking = (req, res) => {
-  const booking = bookings.find((b) => b.id === req.params.id);
-  return !booking
-    ? res.status(404).json({ success: false, message: "Booking not found" })
-    : res.json(booking);
+  const id = req.params.id;
+  connection.query(
+    "SELECT * FROM bookings WHERE booking_id = ?",
+    [id],
+    (err, results) => {
+      return !results
+        ? res.status(404).json({ success: false, message: "Booking not found" })
+        : res.json({ booking: results });
+    }
+  );
 };
 exports.deleteBooking = (req, res) => {
-  const index = bookings.findIndex((b) => b.id === req.params.id);
-  bookings.splice(index, 1);
-  return index < 0
-    ? res.status(404).json({ success: false, message: "Booking not found" })
-    : res.json({ success: true, message: "Booking successfully deleted" });
-};
-exports.updateBooking = (req, res) => {
-  bookings.forEach((booking, index) => {
-    if (booking.id === req.params.id) {
-      booking = booking[index];
-      return !bookings[index]
+  const id = req.params.id;
+  connection.query(
+    "DELETE FROM bookings WHERE booking_id = ?",
+    [id],
+    (err, results) => {
+      return !results
         ? res.status(404).json({ success: false, message: "Booking not found" })
-        : (bookings[index] = req.body);
+        : res.json({
+            success: true,
+            message: "Booking successfully deleted",
+          });
     }
-  });
-  return res.json({ success: true, message: "Booking successfully updated" });
+  );
+};
+
+exports.updateBooking = (req, res) => {
+  const id = req.params.id;
+  connection.query(
+    "UPDATE bookings SET guest_name = ?, order_date = ?, checkin = ?, checkout = ?, special_request = ?, room_id = ?, status = ? WHERE booking_id = ?",
+    [
+      req.body.guest_name,
+      req.body.order_date,
+      req.body.checkin,
+      req.body.checkout,
+      req.body.special_request,
+      req.body.room_id,
+      req.body.status,
+      id,
+    ],
+    (err, results) => {
+      return !results
+        ? res.status(404).json({ success: false, message: "Booking not found" })
+        : res.json({ success: true, message: "Booking successfully updated" });
+    }
+  );
 };
