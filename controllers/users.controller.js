58e5a45943f65716c75e583b999c9902ae516b06
Miriam Torrens <m.torrens14@gmail.com>
2022-07-20 17:33:32 +0200

    users controller whith catch error

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,8 +3,12 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 exports.usersList = async (req, res) => {
-  const users = await User.find();
-  return res.json(users);
+  try {
+    const users = await User.find();
+    return res.json(users);
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
 exports.addUser = async (req, res) => {
   const newUser = new User({
@@ -17,36 +21,53 @@ exports.addUser = async (req, res) => {
     user_image: req.body.user_image,
     password: bcrypt.hashSync(req.body.password, 5),
   });
-  await newUser.save();
-  return res.json({ success: true, message: "User successfully added" });
+  try {
+    await newUser.save();
+    return res.json({ success: true, message: "User successfully added" });
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
+
 exports.getUser = async (req, res) => {
-  const user = await User.findOne({ _id: req.params.id });
-  return !user
-    ? res.status(404).json({ success: false, message: "User not found" })
-    : res.json(user);
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    return !user
+      ? res.status(404).json({ success: false, message: "User not found" })
+      : res.json(user);
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
 exports.deleteUser = async (req, res) => {
-  const user = await User.findOneAndDelete({ _id: req.params.id });
-  return !user
-    ? res.status(404).json({ success: false, message: "User not found" })
-    : res.json({ success: true, message: "User successfully deleted" });
+  try {
+    const user = await User.findOneAndDelete({ _id: req.params.id });
+    return !user
+      ? res.status(404).json({ success: false, message: "User not found" })
+      : res.json({ success: true, message: "User successfully deleted" });
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
 exports.updateUser = async (req, res) => {
-  const user = await User.findOneAndUpdate(
-    { _id: req.params.id },
-    {
-      user_name: req.body.user_name,
-      user_email: req.body.user_email,
-      user_phone: req.body.user_phone,
-      start_date: req.body.start_date,
-      occupation: req.body.occupation,
-      status: req.body.status,
-      user_image: req.body.user_image,
-      password: bcrypt.hashSync(req.body.password, 5),
-    }
-  );
-  return !user
-    ? res.status(404).json({ success: false, message: "User not found" })
-    : res.json({ success: true, message: "User successfully updated" });
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        user_name: req.body.user_name,
+        user_email: req.body.user_email,
+        user_phone: req.body.user_phone,
+        start_date: req.body.start_date,
+        occupation: req.body.occupation,
+        status: req.body.status,
+        user_image: req.body.user_image,
+        password: bcrypt.hashSync(req.body.password, 5),
+      }
+    );
+    return !user
+      ? res.status(404).json({ success: false, message: "User not found" })
+      : res.json({ success: true, message: "User successfully updated" });
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
