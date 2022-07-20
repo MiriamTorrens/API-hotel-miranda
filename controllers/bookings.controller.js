beb65b5a0423d592097ce5bced71ec1086896369
Miriam Torrens <m.torrens14@gmail.com>
2022-07-20 18:03:42 +0200

    bookings and rooms controller catch error

--- controllers/bookings.controller.js
+++ controllers/bookings.controller.js
@@ -2,40 +2,64 @@ require("../db");
 const Booking = require("../models/Booking");
 
 exports.bookingsList = async (req, res) => {
-  const bookings = await Booking.find();
-  return res.json(bookings);
+  try {
+    const bookings = await Booking.find();
+    return res.json(bookings);
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
+
 exports.addBooking = async (req, res) => {
-  const newBooking = new Booking(req.body);
-  await newBooking.save();
-  return res.json({ success: true, message: "Booking successfully added" });
+  try {
+    const newBooking = new Booking(req.body);
+    await newBooking.save();
+    return res.json({ success: true, message: "Booking successfully added" });
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
+
 exports.getBooking = async (req, res) => {
-  const booking = await Booking.findOne({ _id: req.params.id });
-  return !booking
-    ? res.status(404).json({ success: false, message: "Booking not found" })
-    : res.json(booking);
+  try {
+    const booking = await Booking.findOne({ _id: req.params.id });
+    return !booking
+      ? res.status(404).json({ success: false, message: "Booking not found" })
+      : res.json(booking);
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
+
 exports.deleteBooking = async (req, res) => {
-  const booking = await Booking.findOne({ _id: req.params.id });
-  return !booking
-    ? res.status(404).json({ success: false, message: "Booking not found" })
-    : res.json({ success: true, message: "Booking successfully deleted" });
+  try {
+    const booking = await Booking.findOne({ _id: req.params.id });
+    return !booking
+      ? res.status(404).json({ success: false, message: "Booking not found" })
+      : res.json({ success: true, message: "Booking successfully deleted" });
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
+
 exports.updateBooking = async (req, res) => {
-  const booking = await Booking.findOneAndUpdate(
-    { _id: req.params.id },
-    {
-      guest_name: req.body.guest_name,
-      order_date: req.body.order_date,
-      checkin: req.body.checkin,
-      checkout: req.body.checkout,
-      special_request: req.body.special_request,
-      room_id: req.body.room_id,
-      status: req.body.status,
-    }
-  );
-  return !booking
-    ? res.status(404).json({ success: false, message: "Booking not found" })
-    : res.json({ success: true, message: "Booking successfully updated" });
+  try {
+    const booking = await Booking.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        guest_name: req.body.guest_name,
+        order_date: req.body.order_date,
+        checkin: req.body.checkin,
+        checkout: req.body.checkout,
+        special_request: req.body.special_request,
+        room_id: req.body.room_id,
+        status: req.body.status,
+      }
+    );
+    return !booking
+      ? res.status(404).json({ success: false, message: "Booking not found" })
+      : res.json({ success: true, message: "Booking successfully updated" });
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
--- controllers/rooms.controller.js
+++ controllers/rooms.controller.js
@@ -2,41 +2,65 @@ require("../db");
 const Room = require("../models/Room");
 
 exports.roomsList = async (req, res) => {
-  const rooms = await Room.find();
-  return res.json(rooms);
+  try {
+    const rooms = await Room.find();
+    return res.json(rooms);
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
+
 exports.addRoom = async (req, res) => {
-  const newRoom = new Room(req.body);
-  await newRoom.save();
-  return res.json({ success: true, message: "Room successfully added" });
+  try {
+    const newRoom = new Room(req.body);
+    await newRoom.save();
+    return res.json({ success: true, message: "Room successfully added" });
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
+
 exports.getRoom = async (req, res) => {
-  const room = await Room.findOne({ _id: req.params.id });
-  return !room
-    ? res.status(404).json({ success: false, message: "Room not found" })
-    : res.json(room);
+  try {
+    const room = await Room.findOne({ _id: req.params.id });
+    return !room
+      ? res.status(404).json({ success: false, message: "Room not found" })
+      : res.json(room);
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
+
 exports.deleteRoom = async (req, res) => {
-  const room = await Room.findOneAndDelete({ _id: req.params.id });
-  return !room
-    ? res.status(404).json({ success: false, message: "Room not found" })
-    : res.json({ success: true, message: "Booking successfully deleted" });
+  try {
+    const room = await Room.findOneAndDelete({ _id: req.params.id });
+    return !room
+      ? res.status(404).json({ success: false, message: "Room not found" })
+      : res.json({ success: true, message: "Booking successfully deleted" });
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
+
 exports.updateRoom = async (req, res) => {
-  const room = await Room.findOneAndUpdate(
-    { _id: req.params.id },
-    {
-      room_number: req.body.room_number,
-      bed_type: req.body.bed_type,
-      offer: req.body.offer,
-      price: req.body.price,
-      discount: req.body.discount,
-      cancellation: req.body.cancellation,
-      amenities: req.body.amenities,
-      images: req.body.images,
-    }
-  );
-  return !room
-    ? res.status(404).json({ success: false, message: "Room not found" })
-    : res.json({ success: true, message: "Booking successfully updated" });
+  try {
+    const room = await Room.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        room_number: req.body.room_number,
+        bed_type: req.body.bed_type,
+        offer: req.body.offer,
+        price: req.body.price,
+        discount: req.body.discount,
+        cancellation: req.body.cancellation,
+        amenities: req.body.amenities,
+        images: req.body.images,
+      }
+    );
+    return !room
+      ? res.status(404).json({ success: false, message: "Room not found" })
+      : res.json({ success: true, message: "Booking successfully updated" });
+  } catch (err) {
+    return res.status(400).json({ success: false, message: err.message });
+  }
 };
